f391194fcbb6074bf03bf001fd5aed7f988b4a41
Matt D <matt3dixon@gmail.com>
2018-03-20 22:36:06 +1000

    Formatted server

--- server.js
+++ server.js
@@ -13,30 +13,26 @@ const primes = fs.readFileSync(process.cwd()+'/primesupto1000K.ls')
     .map(p => parseInt(p))
     .filter(p => p)
 
-app.get('/',(req,res)=>{
-    res.sendFile(process.cwd()+'/primer.html')
-})
-app.get('/bases',(req,res)=>{
-    res.sendFile(process.cwd()+'/primer.html')
-})
-
-app.get('/canvas.js',(req,res)=>{
-    res.sendFile(process.cwd()+'/canvas.js')
-})
+app.get('/',(req,res)=>res.sendFile(process.cwd()+'/primer.html'))
+app.get('/bin',(req,res)=>res.sendFile(process.cwd()+'/primer.html'))
+app.get('/bases',(req,res)=>res.sendFile(process.cwd()+'/primer.html'))
+app.get('/canvas.js',(req,res)=>res.sendFile(process.cwd()+'/canvas.js'))
+app.get('/primes.json', (req,res)=>res.send(JSON.stringify(primes.slice(0,MAX_NUM_PRIMES))))
 
 app.get('/main.js',(req,res)=>{
     const page = req.query.uri.split('/').slice(-1)[0]
     console.log(page)
     const script = {
         '':'main.js',
+        'bin':'main.js',
         'bases':'bases.js'
     }[page]
     res.sendFile(process.cwd()+`/${script}`)
 })
 
-app.get('/primes.json', (req,res)=>{
-    res.send(JSON.stringify(primes.slice(0,MAX_NUM_PRIMES)))
-})
-
-
+console.log(`Routes: 
+    /       - /bin
+    /bin    - Binary viewer
+    /bases  - Sieve of Eratosthenes viewer
+`)
 app.listen(PORT, ()=> console.log(`We listenin http://127.0.0.1:${PORT}`))
